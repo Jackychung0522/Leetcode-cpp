d02493ca39388b0efa9826bf458c9a1df94abe98
Jackychung0522 <stp77770z@gmail.com>
2025-04-21 00:15:24 +0800

    add 781

---
+++ medium/781.rabbits-in-forest/781.rabbits-in-forest.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=781 lang=cpp
+ *
+ * [781] Rabbits in Forest
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int numRabbits(vector<int> &answers)
+    {
+    }
+};
+// @lc code=end
