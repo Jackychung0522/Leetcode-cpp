310fb71c1c57fc201db3e3bcda2c5ddd7c618dca
Jackychung0522 <stp77770z@gmail.com>
2025-04-26 03:36:50 +0800

    2845

---
+++ medium/2845.count-of-interesting-subarrays/2845.count-of-interesting-subarrays.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2845 lang=cpp
+ *
+ * [2845] Count of Interesting Subarrays
+ */
+
+// @lc code=start
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        
+    }
+};
+// @lc code=end
+
